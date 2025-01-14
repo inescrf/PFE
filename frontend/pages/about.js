68370d6a3f15af46cf7750e5bbef23a38fa3f99b
inescrf <ines.goulamhoussen@yahoo.com>
2025-01-15 00:27:45 +0100

    add header and navbar

---
+++ frontend/pages/about.js
@@ -0,0 +1,3 @@
+export default function About() {
+    return <h1 className="text-center mt-10 text-2xl">Qui sommes-nous</h1>;
+}
---
+++ frontend/pages/contact.js
@@ -0,0 +1,3 @@
+export default function Contact() {
+    return <h1 className="text-center mt-10 text-2xl">Nous contacter</h1>;
+}
